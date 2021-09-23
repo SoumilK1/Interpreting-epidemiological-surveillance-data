34ade8f25d32094202c063abec60fd6e696c792f
SoumilK1 <86109485+SoumilK1@users.noreply.github.com>
2021-09-23 18:23:26 +0530

    Add files via upload

--- src/main/scala/epi_project/testing/Person.scala
+++ src/main/scala/epi_project/testing/Person.scala
@@ -8,21 +8,34 @@ import com.bharatsim.engine.graph.patternMatcher.MatchCondition._
 import com.bharatsim.engine.models.{Agent, Node, StatefulAgent}
 import com.bharatsim.engine.utils.Probability.biasedCoinToss
 import epi_project.testing.InfectionStatus._
+import epi_project.testing.Main.testing
 
-case class Person(id: Long, age: Int, infectionState: InfectionStatus, infectionDur: Int, isTested:String = "false") extends StatefulAgent {
+case class Person(id: Long, age: Int, infectionState: InfectionStatus, infectionDur: Int, beingTested:Int = 0,isEligible:Boolean = false, testStatus:String = "n", lastTestDay:Int = 0) extends StatefulAgent {
 
   private val incrementInfectionDay: Context => Unit = (context: Context) => {
-    if (isPresymptomatic && context.getCurrentStep % Disease.numberOfTicksInADay == 0) {
+    if (isPresymptomatic && context.getCurrentStep % Disease.numberOfTicksInADay == 0)
       updateParam("infectionDur", infectionDur + 1)
-    }
   }
 
-  private val testing:Context => Unit = (context:Context) => {
-    if (isSick && Disease.numberOfTestsDoneAtEachTick < Disease.numberOfTestsAvailable && !Tested && getRecoveredCount(context) >= 2000){
-      updateParam("isTested","true")
+  private val checkEligibilityForTesting:Context => Unit = (context:Context) => {
+    if (getRecoveredCount(context)>2000 && Disease.numberOfTestsDoneAtEachTick < Disease.numberOfTestsAvailable && isSymptomatic && !isBeingTested && !isPositive && isEligibleForTestingAgain(context)){
+      updateParam("isEligible",true)
       Disease.numberOfTestsDoneAtEachTick = Disease.numberOfTestsDoneAtEachTick + 1
     }
   }
+
+  private val declarationOfResults:Context => Unit = (context:Context) => {
+    if (beingTested == 1 && isDelayPeriodOver(context)){
+      if (testStatus == "p"){
+        updateParam("beingTested",2)
+      }
+      if (testStatus == "n"){
+        updateParam("beingTested",0)
+      }
+    }
+  }
+
+
 
   def isSusceptible: Boolean = infectionState == Susceptible
 
@@ -36,10 +49,17 @@ case class Person(id: Long, age: Int, infectionState: InfectionStatus, infection
 
   def isRecovered: Boolean = infectionState == Recovered
 
-  def Tested:Boolean = isTested == "true"
+  def isBeingTested:Boolean = beingTested == 1 || beingTested == 2
 
-  def isSick: Boolean = infectionState == MildlyInfected || infectionState == SeverelyInfected
+  def isSymptomatic: Boolean = infectionState == MildlyInfected || infectionState == SeverelyInfected
 
+  def isPositive:Boolean = testStatus == "p"
+
+  def isEligibleForTesting:Boolean = isEligible
+
+  def isEligibleForTestingAgain(context: Context):Boolean = ((context.getCurrentStep)/Disease.numberOfTicksInADay - lastTestDay >= Disease.daysAfterWhichEligibleForTestingAgain) && lastTestDay != 0
+
+  def isDelayPeriodOver(context: Context):Boolean = (context.getCurrentStep)/Disease.numberOfTicksInADay - lastTestDay >= Disease.testDelay && lastTestDay != 0
 
   private def getRecoveredCount(context: Context) = {
     context.graphProvider.fetchCount("Person", "infectionState" equ Recovered)
@@ -52,11 +72,13 @@ case class Person(id: Long, age: Int, infectionState: InfectionStatus, infection
       case "House" => node.as[House]
       case "Office" => node.as[Office]
       case "School" => node.as[School]
+      case "Hospital" => node.as[Hospital]
     }
   }
 
   addBehaviour(incrementInfectionDay)
-  addBehaviour(testing)
+  addBehaviour(checkEligibilityForTesting)
+  addBehaviour(declarationOfResults)
 
 
   addRelation[House]("STAYS_AT")
